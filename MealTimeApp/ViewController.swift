6f1706174b5de0d65a95033754a08b2ef428e397
Felix Titov <titow.feliks@yandex.ru>
2022-06-13 18:27:20 +0300

    Initial Commit

---
+++ MealTimeApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MealTimeApp
+//
+//  Created by Felix Titov on 6/13/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
